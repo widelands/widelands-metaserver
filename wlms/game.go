164554237806bc0cd31ef344f7566652daf8d3df
Notabilis <notabilis27@web.de>
2017-06-22 07:51:18 +0200

    Added IPv6 support to the metaserver.

--- wlms/game.go
+++ wlms/game.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"time"
+	"net"
 )
 
 type GameState int
@@ -10,7 +11,9 @@ type GameState int
 const (
 	INITIAL_SETUP GameState = iota
 	NOT_CONNECTABLE
-	CONNECTABLE
+	CONNECTABLE_V4
+	CONNECTABLE_V6
+	CONNECTABLE_BOTH
 	RUNNING
 )
 
@@ -27,11 +30,70 @@ type GamePinger struct {
 	C chan bool
 }
 
+func (game *Game) handlePingResult(server *Server, result bool, is_v4 bool) {
+	if result {
+		log.Printf("Successfull ping reply from game %s.", game.Name())
+		switch game.state {
+		case INITIAL_SETUP:
+			if is_v4 {
+				game.SetState(*server, CONNECTABLE_V4)
+			} else {
+				game.SetState(*server, CONNECTABLE_V6)
+			}
+		case NOT_CONNECTABLE:
+			if is_v4 {
+				game.SetState(*server, CONNECTABLE_V4)
+			} else {
+				game.SetState(*server, CONNECTABLE_V6)
+			}
+		case CONNECTABLE_V4:
+			if !is_v4 {
+				game.SetState(*server, CONNECTABLE_BOTH)
+			}
+		case CONNECTABLE_V6:
+			if is_v4 {
+				game.SetState(*server, CONNECTABLE_BOTH)
+			}
+		case CONNECTABLE_BOTH, RUNNING:
+			// Do nothing
+		default:
+			log.Fatalf("Unhandled game.state: %v", game.state)
+		}
+	} else {
+		log.Printf("Failed ping reply from game %s.", game.Name())
+		switch game.state {
+		case INITIAL_SETUP:
+			game.SetState(*server, NOT_CONNECTABLE)
+		case NOT_CONNECTABLE:
+			// Do nothing.
+		case CONNECTABLE_V4:
+			if is_v4 {
+				game.SetState(*server, NOT_CONNECTABLE)
+			}
+		case CONNECTABLE_V6:
+			if !is_v4 {
+				game.SetState(*server, NOT_CONNECTABLE)
+			}
+		case CONNECTABLE_BOTH:
+			if is_v4 {
+				game.SetState(*server, CONNECTABLE_V6)
+			} else {
+				game.SetState(*server, CONNECTABLE_V4)
+			}
+		case RUNNING:
+			// Do nothing
+		default:
+			log.Fatalf("Unhandled game.state: %v", game.state)
+		}
+	}
+}
+
 func (game *Game) pingCycle(server *Server) {
 	// Remember to remove the game when we no longer receive pings.
 	defer server.RemoveGame(game)
 
 	first_ping := true
+	ping_primary_ip := true
 	for {
 		// This game is not even in our list anymore. Give up. If the game has no
 		// host anymore or it has disconnected, remove the game.
@@ -46,37 +108,37 @@ func (game *Game) pingCycle(server *Server) {
 		if first_ping {
 			pingTimeout = server.GameInitialPingTimeout()
 		}
-		first_ping = false
 
-		pinger := server.NewGamePinger(host, pingTimeout)
-		success, ok := <-pinger.C
-		if success && ok {
-			log.Printf("Successfull ping reply from game %s.", game.Name())
-			switch game.state {
-			case INITIAL_SETUP:
-				host.SendPacket("GAME_OPEN")
-				game.SetState(*server, CONNECTABLE)
-			case NOT_CONNECTABLE:
-				game.SetState(*server, CONNECTABLE)
-			case CONNECTABLE, RUNNING:
-				// Do nothing
-			default:
-				log.Fatalf("Unhandled game.state: %v", game.state)
-			}
-		} else {
-			log.Printf("Failed ping reply from game %s.", game.Name())
-			switch game.state {
-			case INITIAL_SETUP:
-				host.SendPacket("ERROR", "GAME_OPEN", "GAME_TIMEOUT")
-				game.SetState(*server, NOT_CONNECTABLE)
-			case NOT_CONNECTABLE:
-				// Do nothing.
-			case CONNECTABLE, RUNNING:
-				return
-			default:
-				log.Fatalf("Unhandled game.state: %v", game.state)
-			}
+		connected := false
+
+		// The idea is to alternate between pinging the two IP addresses of the client, except for the
+		// first round or if there is only one IP address
+		if ping_primary_ip || host.otherIp() == "" {
+			// Primary IP
+			pinger := server.NewGamePinger(host.remoteIp(), pingTimeout)
+			success, ok := <-pinger.C
+			connected = success && ok
+			game.handlePingResult(server, connected, net.ParseIP(host.remoteIp()).To4() != nil)
 		}
+		if (!ping_primary_ip || first_ping) && host.otherIp() != "" {
+			// Secondary IP
+			pinger := server.NewGamePinger(host.otherIp(), pingTimeout)
+			success, ok := <-pinger.C
+			connected = success && ok
+			game.handlePingResult(server, connected, net.ParseIP(host.otherIp()).To4() != nil)
+
+		}
+		if first_ping {
+			// On first ping, inform the client about the result
+			if connected {
+				host.SendPacket("GAME_OPEN")
+			} else {
+				host.SendPacket("ERROR", "GAME_OPEN", "GAME_TIMEOUT")
+			}
+			first_ping = false
+		}
+
+		ping_primary_ip = !ping_primary_ip
 		time.Sleep(server.GamePingTimeout())
 	}
 }
